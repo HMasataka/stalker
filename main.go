cd96bbe993a6ef6e300cad47b417311e43a379e1
HMasataka <siruba.2050@gmail.com>
2022-12-16 17:36:42 +0900

    Feat wrap error

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	fmt.Println(e())
+	fmt.Println(Wrap(e()))
 }
 
 func e() error {
@@ -18,19 +18,46 @@ func e() error {
 func New(msg string) error {
 	_, file, line, _ := runtime.Caller(1)
 
-	return &Error{
-		err:  errors.New(msg),
-		file: file,
-		line: line,
+	return &errs{
+		single{
+			err:  errors.New(msg),
+			file: file,
+			line: line,
+		},
 	}
 }
 
-type Error struct {
+func Wrap(err error) error {
+	_, file, line, _ := runtime.Caller(1)
+
+	return &errs{
+		single{
+			err:  err,
+			file: file,
+			line: line,
+		},
+	}
+}
+
+type single struct {
 	err  error
 	file string
 	line int
 }
 
-func (e *Error) Error() string {
-	return fmt.Sprintf("%v %v %v", e.err, e.file, e.line)
+type errs []single
+
+func (es errs) Error() string {
+	var res string
+	for _, e := range es {
+		res += fmt.Sprintf("%v %v %v\n", e.err, e.file, e.line)
+	}
+	return res
+}
+
+func (es errs) Unwrap() error {
+	if len(es) == 0 {
+		return nil
+	}
+	return es[1:]
 }
