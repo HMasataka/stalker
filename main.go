7b982156710d6ebd2391f9ac7bd51e45edbe3913
HMasataka <siruba.2050@gmail.com>
2022-12-16 18:03:01 +0900

    Refactor errors

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	fmt.Println(Wrap(e()))
+	w := Wrap(e())
+	var e es
+	fmt.Println(errors.As(w, &e))
+	fmt.Println(e)
+
+	fmt.Println(w)
 }
 
 func e() error {
@@ -18,46 +23,35 @@ func e() error {
 func New(msg string) error {
 	_, file, line, _ := runtime.Caller(1)
 
-	return &errs{
-		single{
-			err:  errors.New(msg),
-			file: file,
-			line: line,
-		},
+	return es{
+		err:  errors.New(msg),
+		file: file,
+		line: line,
 	}
 }
 
 func Wrap(err error) error {
 	_, file, line, _ := runtime.Caller(1)
 
-	return &errs{
-		single{
-			err:  err,
-			file: file,
-			line: line,
-		},
+	return es{
+		err:  err,
+		file: file,
+		line: line,
 	}
 }
 
-type single struct {
+type es struct {
 	err  error
 	file string
 	line int
 }
 
-type errs []single
-
-func (es errs) Error() string {
+func (e es) Error() string {
 	var res string
-	for _, e := range es {
-		res += fmt.Sprintf("%v %v %v\n", e.err, e.file, e.line)
-	}
+	res += fmt.Sprintf("%v %v %v\n", e.err, e.file, e.line)
 	return res
 }
 
-func (es errs) Unwrap() error {
-	if len(es) == 0 {
-		return nil
-	}
-	return es[1:]
+func (e es) Unwrap() error {
+	return e.err
 }
